0abccd6fc88d8c54398b4ef272cc92165b0204f3
altafahmadfakir <altafahmadf775@gmail.com>
2026-07-19 12:43:16 +0545

    Revise greeting and warning messages in main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
-st.title("my first streamlit App")
+st.title("hey Altaf")
 name=st.text_input("Enter your name")
 if st.button("submit"):
     if name:
-        st.success(f"hello,{name}!welcome to streamlit,")
+        st.success(f"hello,{name}!welcome to my app,")
     else:
         st.warning("p-lease enter your nmae")
