8e823b00c922a379cda16165ca37ff779a6780d6
altafahmadfakir <altafahmadf775@gmail.com>
2026-07-19 13:03:24 +0545

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.title("hey Altaf")
-name=st.text_input("Enter your name")
-if st.button("submit"):
-    if name:
-        st.success(f"hello,{name}!welcome to my app,")
-    else:
-        st.warning("p-lease enter your nmae")
